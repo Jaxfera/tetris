7c60a062600a3cc70621a8d5ee25d6c1cbc6dde6
ThreeDPrinter <manuel.stoer@outlook.com>
2019-01-18 18:59:29 +0100

    Added playfield drawing

--- main.cpp
+++ main.cpp
@@ -1,24 +1,42 @@
-#include <iostream>
 #include <ncurses.h>
+
+struct Piece {
+};
 
 int main()
 {
-    int input_delay_decisecond = 500;
-    int user_input;
-    bool is_running = true;
+  int input_delay_decisecond = 10;
+  int user_input;
+  bool is_running = true;
+  
+  /* Init curses */
+  initscr();
+  noecho();
+  keypad(stdscr, true);
+  halfdelay(input_delay_decisecond);
+  curs_set(0);
+  
+  /* Create playfield */
+  WINDOW* playfield = newwin(17, 9, 0, 0);
+  box(playfield, 0, 0);
+  
+  /* Game loop */
+  while(is_running) {
+	 refresh();
+	 wrefresh(playfield);
 
-    /* Init curses */
-    initscr();
-    noecho();
-    keypad(stdscr, true);
-    halfdelay(input_delay_decisecond);
-
-    /* Game loop */
-    while(is_running) {
-        user_input = getch();
-        addch(user_input);
-        refresh();
-    }
-
-    endwin();
+	 switch(getch()) {
+	 case KEY_LEFT:
+		break;
+	 case KEY_RIGHT:
+		break;
+	 case KEY_UP:
+		break;
+	 case KEY_DOWN:
+		break;
+	 }
+  }
+  
+  delwin(playfield);
+  endwin();
 }
